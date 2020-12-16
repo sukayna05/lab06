73b28a878ede45d6c96229959273ddcacd5884eb
sukaynaDib <sukayna05@gmail.com>
2020-12-16 20:33:29 +0200

    inhancing the readibility

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 
-var studentName = prompt ("what is you name?"); 
-alert ("Hello  " + studentName);
 
-var university = prompt("which university you are attending?");
-alert ("great!")
 
-var uniNumber = prompt ("what is you university number?");{
-  
-    if (uniNumber > 1000 ) {
-        alert ("plz sign up");
-    
-    }else {
-        alert ("re write your correct university number")
-    }
+
+function greetingstudentName(){
+    var studentName = prompt("what is you name?"); 
+    alert ("Hello  " + studentName);
 }
 
+greetingstudentName();
+
+
+//
+
+function welcomUnistudent(){
+var university = prompt("which university you are attending?");
+alert ("more than welcome");
+}
+
+welcomUnistudent();
+
+//
+
+function checkUninumber(){
+
+    var uniNumber = prompt("what is you university number?");{
+  
+        if (uniNumber > 1000 ) {
+            alert ("plz sign up first");
+        
+        }else {
+            alert (" re write your correct university number");
+        }
+    }
+
+}
